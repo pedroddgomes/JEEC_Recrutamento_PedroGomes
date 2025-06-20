9a9ae52fef2153ea43b0af3a07f4f364b2c323a8
Pedro Gomes <pedroddgomes@tecnico.ulisboa.pt>
2025-06-21 00:02:05 +0100

    clean

--- jeec_app/services/example/delete_example_service.py
+++
@@ -1,9 +0,0 @@
-from jeec_app.models.example import Example
-
-class DeleteExampleService:
-    def __init__(self, example: Example):
-        self.example = example
-
-    def call(self) -> bool:
-        result = self.example.delete()
-        return result
